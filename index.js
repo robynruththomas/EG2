bc6944c08c9eb2bc00bc4195f634c6691177ffa4
Robyn <robyn@codeforte.com>
2018-02-12 15:44:23 +0200

    Change Port

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var express 	= require("express");
    bodyParser   = require("body-parser"),
    indexRoutes 	= require('./routes/index');
-   
+
 var app = express();
+var port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use(indexRoutes);
 
-app.listen(3000, function() {
-    console.log("server is on!")
-});
 
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
